9ed956fb54521b949f69e332e679c7b87f9e4027
Franz-Josef Färber <franz-josef.faerber@stmuk.bayern.de>
2025-10-27 11:51:08 +0100

    DOKU_DISABLE_GZIP_OUTPUT: ifndef

--- lib/exe/taskrunner.php
+++ lib/exe/taskrunner.php
@@ -12,7 +12,9 @@ use dokuwiki\TaskRunner;
 if (!defined('DOKU_INC')) {
     define('DOKU_INC', __DIR__ . '/../../');
 }
-define('DOKU_DISABLE_GZIP_OUTPUT', 1);
+if (!defined('DOKU_DISABLE_GZIP_OUTPUT')) {
+    define('DOKU_DISABLE_GZIP_OUTPUT', 1);
+}
 require_once DOKU_INC . 'inc/init.php';
 session_write_close();  //close session
 
